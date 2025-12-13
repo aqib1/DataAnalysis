eb9262301811b01febf477c1909cb2e5bcb796d3
aqib javed <aqibbutt3078@gmail.com>
2025-12-13 22:46:08 +0000

    Adding unit tests

---
+++ TestCalculations.py
@@ -0,0 +1,18 @@
+import unittest
+from code.calculations import Calculations
+
+class TestCalculation(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.calculation = Calculations(8, 2)
+
+    def test_sum(self):
+        self.assertEqual(self.calculation.get_sum(), 10)
+
+    def test_diff(self):
+        self.assertEqual(self.calculation.get_difference(), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
